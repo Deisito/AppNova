59ff24a8bd80a5a22c8e6502e213ab8e89266eef
Deisito <calderondavid060@gmail.com>
2023-11-26 19:05:44 -0500

    Primera Versión

---
+++ app/src/main/java/com/example/myloginnova/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.myloginnova
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myloginnova.ui.theme.MyLoginNovaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyLoginNovaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyLoginNovaTheme()
+        Greeting("Android")
+    }
+
+fun MyLoginNovaTheme() {
+    TODO("Not yet implemented")
+}
